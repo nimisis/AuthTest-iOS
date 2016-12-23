6998bef00efedbff10bc8aeeee0f2afd8b51ce76
dave <davebutler257@gmail.com>
2016-12-23 11:37:10 +0000

    nimcake changed to nimisis

--- btoken2.php
+++ btoken2.php
@@ -3,5 +3,5 @@ $p = "";
 if (isset($_POST)) {
 	$p = implode($_POST);
 }
-echo "<head><meta http-equiv=\"refresh\" content=\"0; url=http://nimcake/btoken.php?p=".$p."\" /></head>";
+echo "<head><meta http-equiv=\"refresh\" content=\"0; url=http://nimisis.com/btoken.php?p=".$p."\" /></head>";
 ?>
